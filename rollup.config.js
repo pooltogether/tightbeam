c29ec722bad2b6329cf7e2d04cce496f3a3802d9
Brendan Asselstine <mail.asselstine@gmail.com>
2019-11-27 13:06:05 -0800

    Altered Rollup to output directly in library directory

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,19 @@ import typescript from 'rollup-plugin-typescript';
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/bundle.js',
+    dir: '.',
     format: 'cjs'
   },
+  preserveModules: true,
   external: [
     'apollo-client',
     'apollo-cache-inmemory',
     'graphql-tag',
+    'graphql',
     'ethers',
+    'ethers/utils/interface',
+    'ethers/utils',
+    'ethers/contract',
     'date-fns',
     'lodash'
   ],
